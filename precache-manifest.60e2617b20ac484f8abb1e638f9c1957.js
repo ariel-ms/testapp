d7d96848653350bf8293311fc35a89e0bf879ca5
Ariel Mendez <arielmendez735@gmail.com>
2019-12-26 23:02:26 -0600

    Updates

---
+++ precache-manifest.60e2617b20ac484f8abb1e638f9c1957.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f47437c287f82c8f18e7b27e3a45bf4",
+    "url": "/testapp/index.html"
+  },
+  {
+    "revision": "d0bcfe6a81cbff67096e",
+    "url": "/testapp/static/css/main.88e8ac8f.chunk.css"
+  },
+  {
+    "revision": "dcd2657b3ef8f78d14f4",
+    "url": "/testapp/static/js/2.e6fbc2c9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/testapp/static/js/2.e6fbc2c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d0bcfe6a81cbff67096e",
+    "url": "/testapp/static/js/main.d11e0a3e.chunk.js"
+  },
+  {
+    "revision": "67e6b62ba0b64e8e8abe",
+    "url": "/testapp/static/js/runtime-main.f127ec0a.js"
+  }
+]);
